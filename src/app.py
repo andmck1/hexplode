7510f7c751041dd17fbe3441f20a1d8d04b537b7
andmck <alastair.amhamilton@icloud.com>
2024-07-25 16:54:10 +0100

    exp: added simple gui functionality

--- src/app.py
+++ src/app.py
@@ -1,45 +1,105 @@
 from textual.app import App, ComposeResult
+from rich.panel import Panel
+from textual.reactive import reactive, Reactive
+from textual.widgets import Static, TextArea
 from textual import events
-from textual.widgets import TextArea
 from game import Hexplode
 
 
 hexplode = Hexplode(size=2)
 
 
-class Board(TextArea):
-    text = ""
+class PlayerPanel(Static):
+    score: Reactive[int] = reactive(0)
 
-    @staticmethod
-    def generate_board_text():
-        board = hexplode.create_board()
-        board_text = ""
-        for row in board:
-            board_text += "|"
-            for val in row:
-                board_text += str(val) if val is not None else " "
-                board_text += "|"
-            board_text += "\n"
-        return board_text
+    def __init__(self, player_name: str, colour: str, **textual_kwargs):
+        super().__init__(**textual_kwargs)
+        self.player_name = player_name
+        self.colour = colour
+
+    def on_mount(self):
+        self.update_panel()
+
+    def increment_score(self):
+        self.score += 1
+        self.update_panel()
+
+    def update_panel(self):
+        self.update(Panel(f"[{self.colour}]{self.player_name}: {self.score}"))
+
+
+class BoardPanel(Static):
+    board = ""
+
+    def on_mount(self):
+        self.update_panel()
+
+    def update_panel(self):
+        self.update(Panel("Hello There"))
+
+
+class Board(TextArea):
+    def on_key(self, event: events.Key) -> None:
+        current_text = self.text.split("\n")
+        current_row, current_column = self.cursor_location
+        if event.key == "up":
+            if current_row - 1 >= 0:
+                if (
+                    current_text[current_row - 1][current_column]
+                    in "0123456789"
+                ):
+                    self.move_cursor_relative(columns=0, rows=-1)
+        if event.key == "down":
+            if current_row + 1 < len(current_text):
+                if (
+                    current_text[current_row + 1][current_column]
+                    in "0123456789"
+                ):
+                    self.move_cursor_relative(columns=0, rows=1)
+        if event.key == "left":
+            if current_column - 2 >= 0:
+                if (
+                    current_text[current_row][current_column - 2]
+                    in "0123456789"
+                ):
+                    self.move_cursor_relative(columns=-2, rows=0)
+        if event.key == "right":
+            if current_column + 2 < len(current_text[current_row]):
+                if (
+                    current_text[current_row][current_column + 2]
+                    in "0123456789"
+                ):
+                    self.move_cursor_relative(columns=2, rows=0)
+        if event.key == "enter":
+            self.move_cursor((0, 0))
+        event.prevent_default()
+
+
+class Screen(App):
+    CSS_PATH = "css/screen_layout.tcss"
+
+    # board: BoardPanel = BoardPanel(classes="box", id="board")
+    player_1: PlayerPanel = PlayerPanel(
+        player_name="big man", colour="red", classes="player"
+    )
+    player_2: PlayerPanel = PlayerPanel(
+        player_name="lonk", colour="blue", classes="player"
+    )
+    board: Board | None = None
 
     def compose(self) -> ComposeResult:
-        board_text = self.generate_board_text()
-        self.text = board_text
-        yield TextArea(self.text, read_only=True)
+        self.board = Board("|0|0|0|\n|0|0|0|\n|0|0|0|", read_only=True)
+        self.board.cursor_blink = False
+        self.board.cursor_location = (0, 1)
+        yield self.board
+        yield self.player_1
+        yield self.player_2
 
     def on_key(self, event: events.Key) -> None:
-        if event.key == "e":
-            location = self.cursor_location
-            hexplode.make_move((0, 0, 0))
-            board_text = self.generate_board_text()
-            self.text = board_text
+        if event.key == "space":
+            self.player_1.increment_score()
 
 
-class HexplodeApp(App):
-    def compose(self):
-        yield Board()
-
-
-app = HexplodeApp()
 if __name__ == "__main__":
+    app = Screen()
     app.run()
--- src/gui.py
+++ src/gui.py
@@ -1,41 +1,58 @@
 from rich.panel import Panel
 from textual.app import App, ComposeResult
-from textual.reactive import reactive
+from textual.reactive import reactive, Reactive
 from textual.widgets import Static
 from textual.events import Key
-from dataclasses import dataclass
 
 
-class PlayerScore:
-    name: str
-    colour: str
-    score: int = reactive(0)
+class PlayerPanel(Static):
+    score: Reactive[int] = reactive(0)
 
-
-
-
-
-class PlayerScorePanel(Static):
-    score: int = reactive(0)
-    name: str
-    colour: str
-
-    def __init__(self, name: str, colour: str):
-        self.name = name
+    def __init__(self, player_name: str, colour: str, **textual_kwargs):
+        super().__init__(**textual_kwargs)
+        self.player_name = player_name
         self.colour = colour
 
-    def on_mount(self) -> None:
-        self.update(Panel(f"[bold {self.colour}]Player 1: {self.score}"))
+    def on_mount(self):
+        self.update_panel()
+
+    def increment_score(self):
+        self.score += 1
+        self.update_panel()
+
+    def update_panel(self):
+        self.update(Panel(f"[{self.colour}]{self.player_name}: {self.score}"))
 
 
-class BoardPanel:
-    pass
+class BoardPanel(Static):
+    board: Reactive[str] = reactive("")
+
+    def on_mount(self):
+        self.update_panel()
+
+    def update_panel(self):
+        self.update(Panel("0000\n" "0000\n" "0000\n"))
 
 
 class Screen(App):
+    CSS_PATH = "css/screen_layout.tcss"
+
+    board: BoardPanel = BoardPanel(classes="box", id="board")
+    player_1: PlayerPanel = PlayerPanel(
+        player_name="big man", colour="red", classes="player"
+    )
+    player_2: PlayerPanel = PlayerPanel(
+        player_name="lonk", colour="blue", classes="player"
+    )
+
     def compose(self) -> ComposeResult:
-        PlayerScorePanel(name="Player 1", colour="blue")
-        PlayerScorePanel(name="Player 2", colour="red")
+        yield self.board
+        yield self.player_1
+        yield self.player_2
+
+    def on_key(self, event: Key) -> None:
+        if event.key == "space":
+            self.player_1.increment_score()
 
 
 if __name__ == "__main__":
